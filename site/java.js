5c6093ded43db4f484bfb428f19fa063716c3b01
RicardopcJunior <ricardo.paulo.cjr@hotmail.com>
2026-05-15 13:10:10 -0300

    3

---
+++ site/java.js
@@ -0,0 +1,8 @@
+const zap = document.querySelector(".botao");
+
+zap.addEventListener("click", function () {
+    window.open(
+        "https://wa.me/5511969303406?text=Quero%20fazer%20um%20pedido",
+        "_blank"
+    );
+});
